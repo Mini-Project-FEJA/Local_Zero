1908a047e970944426894a563d8f60862e1d33b4
fridalarsson13 <fridalarsson25@gmail.com>
2026-04-14 07:02:17 +0200

    controller

--- localzero/backend/src/main/java/feja/localzero/controller/AppController.java
+++ localzero/backend/src/main/java/feja/localzero/controller/AppController.java
@@ -1,4 +1,4 @@
-package feja.localzero;
+package feja.localzero.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
