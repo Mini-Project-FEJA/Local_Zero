57954f4f9f9155b2720bfc9db0546359b1ab0b2b
joarsm <joarmagnu@hotmail.com>
2026-04-27 10:47:14 +0200

    bara testar en sak

--- localzero/backend/src/main/resources/static/frontpage.js
+++ localzero/backend/src/main/resources/static/frontpage.js
@@ -1,1 +1,21 @@
 //All JavaScript för frontpage
+
+
+const usernameField = document.getElementById("account-box-name");
+const savedUser = localStorage.getItem("user");
+
+if (usernameField && savedUser) {
+    const userObject = JSON.parse(savedUser);
+    var username = userObject.username;
+
+    usernameField.textContent = "Logged in as: " + username;
+}
+
+const logoutButton = document.getElementById("account-box-logout-button");
+
+logoutButton.addEventListener("click", function () {
+    localStorage.removeItem("user");
+
+    console.log("Du har loggat ut");
+    window.location.href = "loginpage.html";
+})
